f987cb0a7511e4d27ce77329e3b6bbbeb707b59e
Josh Jones <jones.joshua.d@gmail.com>
2025-03-28 17:37:16 -0500

    Fix Clerk user object access in views and service
    - Update home/index view to use method calls instead of hash access
    - Fix ClerkMemberService to properly access Clerk user properties
    - Update header partial for consistent user property access
    - Resolve NoMethodError when accessing clerk.user properties

--- app/services/clerk_member_service.rb
+++ app/services/clerk_member_service.rb
@@ -1,29 +1,29 @@
 # app/services/clerk_member_service.rb
 class ClerkMemberService
-    def self.create_or_update_from_clerk_user(clerk_user)
-      # Find existing member by clerk_user_id or email
-      member = Member.find_by(clerk_user_id: clerk_user['id']) || 
-               Member.find_by(email: clerk_user['email_addresses'].find { |e| e['id'] == clerk_user['primary_email_address_id'] }['email_address'])
-      
-      if member
-        # Update member information if it exists
-        member.update(
-          first_name: clerk_user['first_name'],
-          last_name: clerk_user['last_name'],
-          email: clerk_user['email_addresses'].find { |e| e['id'] == clerk_user['primary_email_address_id'] }['email_address']
-        )
-      else
-        # Create new member
-        member = Member.create(
-          clerk_user_id: clerk_user['id'],
-          first_name: clerk_user['first_name'],
-          last_name: clerk_user['last_name'],
-          email: clerk_user['email_addresses'].find { |e| e['id'] == clerk_user['primary_email_address_id'] }['email_address'],
-          property_address: '',
-          account_status: 'pending'
-        )
-      end
-      
-      member
+  def self.create_or_update_from_clerk_user(clerk_user)
+    # Find existing member by clerk_user_id or email
+    member = Member.find_by(clerk_user_id: clerk_user.id) || 
+             Member.find_by(email: clerk_user.email_addresses.find { |e| e.id == clerk_user.primary_email_address_id }.email_address)
+    
+    if member
+      # Update member information if it exists
+      member.update(
+        first_name: clerk_user.first_name,
+        last_name: clerk_user.last_name,
+        email: clerk_user.email_addresses.find { |e| e.id == clerk_user.primary_email_address_id }.email_address
+      )
+    else
+      # Create new member
+      member = Member.create(
+        clerk_user_id: clerk_user.id,
+        first_name: clerk_user.first_name,
+        last_name: clerk_user.last_name,
+        email: clerk_user.email_addresses.find { |e| e.id == clerk_user.primary_email_address_id }.email_address,
+        property_address: '',
+        account_status: 'pending'
+      )
     end
+    
+    member
   end
+end
